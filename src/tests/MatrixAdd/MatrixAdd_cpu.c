e284c60ca85b983a4ea72204ccf198174ddbf45c
Mitch-R <70560907+M-Russell-Code@users.noreply.github.com>
2021-03-29 22:59:39 -0400

    Add files via upload

--- src/tests/MatrixAdd/MatrixAdd_cpu.c
+++ src/tests/MatrixAdd/MatrixAdd_cpu.c
@@ -7,23 +7,8 @@
 #define mat_element(mat, cols, row_idx, col_idx) \
   mat[(row_idx * cols) + col_idx]
   
-//initialize the matrix
-ushort mat_init(int rows, int cols)
-{
-  if(rows <= 0 || cols <= 0)
-  {
-    return NULL;
-  }
-  ushort A;
-  A = (ushort)safe_malloc(sizeof(struct matrix));
-  A->cols = cols;
-  A->rows = rows; 
-  A->data = (ushort *)safe_malloc(rows*cols*sizeof(ushort)); 
-  return A;
-}
-
 //Set the value of matix element at position given by the indices to "val"
-void set_matrix_element(ushort A, int row_idx, int col_idx, ushort val)
+void set_matrix_element(bin_matrix A, int row_idx, int col_idx, bin_matrix val)
 {
   if(row_idx < 0 || row_idx >= A->rows || col_idx < 0 || col_idx >= A->cols)
   {
@@ -33,14 +18,14 @@ void set_matrix_element(ushort A, int row_idx, int col_idx, ushort val)
   mat_element(A, row_idx, col_idx) = val;
 }
 
-ushort matrix_add(ushort *A, ushort *B)
+bin_matrix MatrixAdd_cpu(bin_matrix *A, bin_matrix *B)
 {
-    if(A->rows != B->rows || A->cols != B->cols)
-    {
-        printf("Incompatible dimensions for matrix addition. \n");
-        exit(0);
-    }
-    ushort temp mat_init(A->rows, A->cols);
+  //  if(A->rows != B->rows || A->cols != B->cols)
+  //   {
+  //       printf("Incompatible dimensions for matrix addition. \n");
+  //      exit(0);
+  //  }
+  // bin_matrix temp mat_init(A->rows, A->cols);
     for(int i = 0; i < A->rows; i++)
     {
         for(int j = 0; j < A->cols; j++)
--- src/tests/MatrixAdd/sources.cmake
+++ src/tests/MatrixAdd/sources.cmake
@@ -1,3 +1,3 @@
 add_lab("ADD_MATRIX_test")
-add_lab_solution("ADD_MATRIX_test", ${CMAKE_CURRENT_LIST_DIR}/main.cu)
-add_generator("ADD_MATRIX_test" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
+add_lab_solution("ADD_MATRIX_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
+#add_generator("ADD_MATRIX_test", ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
