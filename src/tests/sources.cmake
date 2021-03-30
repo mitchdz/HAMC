de63d9f7ee7403bbe669f34ba9fbf174f6aa301d
Mitch-R <70560907+M-Russell-Code@users.noreply.github.com>
2021-03-29 23:00:48 -0400

    Update sources.cmake

--- src/tests/sources.cmake
+++ src/tests/sources.cmake
@@ -1,1 +1,3 @@
 include(${CMAKE_CURRENT_LIST_DIR}/RREFMatrix/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/MatrixAdd/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/InverseMatrix/sources.cmake)
