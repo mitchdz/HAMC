39b87259a8eabab8b8b51951e1c8aa02b24504af
Mitch-R <70560907+M-Russell-Code@users.noreply.github.com>
2021-03-30 21:20:33 -0400

    Update dataset_generator.cpp

--- src/tests/MultiplyMatrix/dataset_generator.cpp
+++ src/tests/MultiplyMatrix/dataset_generator.cpp
@@ -2,132 +2,94 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "wb.h"
-#include "Multiply_cpu.c"
+#include "../../hamc/hamc_cpu_code.c"
 
 #define ushort unsigned short
 
-#define mat_element(mat, row_idx, col_idx) \
-    mat->data[row_idx * (mat->cols) + col_idx]
-
-typedef struct matrix
-{
-   int rows;             //number of rows.
-   int cols;             //number of columns.
-   ushort *data;
-}*bin_matrix;
-
-void* safe_malloc(size_t n)
-{
-    void* p = malloc(n);
-    if (!p)
-    {
-        fprintf(stderr, "Out of memory(%lu bytes)\n",(size_t)n);
-        exit(EXIT_FAILURE);
-    }
-    return p;
-}
-
-bin_matrix mat_init(int rows, int cols){
-	if(rows <= 0 || cols <= 0)
-		return NULL;
-
-	bin_matrix A;
-	A = (bin_matrix)safe_malloc(sizeof(struct matrix));
-	A->cols = cols;
-	A->rows = rows; 
-	A->data = (ushort *)safe_malloc(rows*cols*sizeof(ushort)); 
-	return A;
-}
-
 static char *base_dir;
 
-static void compute(ushort *output, ushort *input0, ushort *input1, int numARows, int numACols, int numBRows, int numBCols, int numCRows, int numCCols)
-{
-    bin_matrix A = mat_init(numARows, numACols);
-    bin_matrix B = mat_init(numBRows, numBCols);
-    bin_matrix C = mat_init(numCRows, numCCols);
-    
-    A->data = input0;
-    B->data = input1;
-    
-    C = mult_matrix(A, B);
-    output = C->data;
-    
-    free(A);
-    free(B);
-    free(C);
+static ushort *generate_data(int height, int width) {
+  ushort *data = (ushort *)malloc(sizeof(ushort) * width * height);
+  int i;
+  for (i = 0; i < width * height; i++) {
+    //data[i] = ((ushort)(rand() % 20) - 5) / 5.0f;
+    data[i] = (ushort)(rand() % 2);
+  }
+  return data;
 }
 
-static ushort *generate_data(int height, int width)
-{
-    ushort *data = (ushort *)malloc(sizeof(ushort) * width * height);
-    int i;
-    for (i = 0; i < width * height; i++) {
-        data[i] = (ushort)(rand() % 2);
+static void write_data(char *file_name, ushort *data, int height,
+                       int width) {
+  int ii, jj;
+  printf("Start!");
+  FILE *handle = fopen(file_name, "w");
+  fprintf(handle, "%d %d\n", height, width);
+  for (ii = 0; ii < height; ii++) {
+    for (jj = 0; jj < width; jj++) {
+      fprintf(handle, "%.2hi", *data++);
+      if (jj != width - 1) fprintf(handle, " ");
     }
-    return data;
+    if (ii != height - 1) fprintf(handle, "\n");
+  }
+  fflush(handle);
+  fclose(handle);
 }
 
-static void write_data(char *file_name, ushort *data, int height, int width)
-{
-    int ii, jj;
-    FILE *handle = fopen(file_name, "w");
-    fprintf(handle, "%d %d\n", height, width);
-    for(ii = 0; ii < height; ii++){
-        for(jj = 0; jj < width; jj++){
-            fprintf(handle, "%.2hi", *data++);
-            if(jj != width - 1){
-                fprintf(handle, " ");
-            }
-        }
-        if(ii != height - 1){
-            fprintf(handle, "\n");
-        }
-    }
-    fflush(handle);
-    fclose(handle);
-}
-
-static void create_dataset(int datasetNum, int numARows, int numACols, int numBCols)
-{
-    int numBRows = numACols;
+static void create_dataset(int datasetNum, int numARows, int numACols,int numBRows, int numBCols) {
     int numCRows = numARows;
-    int numCCols = numBCols;
-
-    const char *dir_name = wbDirectory_create(wbPath_join(base_dir, datasetNum));
-
+    int numCCols = numACols;
+    
+    printf("Setting Up Directory Name \n"); // GOOD!
+    const char *dir_name =
+        wbDirectory_create(wbPath_join(base_dir, datasetNum));
+	
+    printf("Setting Up Files \n"); // GOOD!
     char *input0_file_name = wbPath_join(dir_name, "input0.raw");
+    printf("input0_file_name -> %s \n", input0_file_name);
     char *input1_file_name = wbPath_join(dir_name, "input1.raw");
     char *output_file_name = wbPath_join(dir_name, "output.raw");
-
+   
+    printf("Filling Up Files \n"); // GOOD!
     ushort *input0_data = generate_data(numARows, numACols);
     ushort *input1_data = generate_data(numBRows, numBCols);
     ushort *output_data = (ushort *)calloc(sizeof(ushort), numCRows * numCCols);
+    
+    printf("Filling Up Matricies \n"); // GOOD!
+    // Create Matricies
+    
+    bin_matrix A = mat_init_cpu(numARows, numACols);
+    A->data = input0_data;
+    printf("Matrix A ready for solution! \n"); // GOOD!
+    
+    bin_matrix B = mat_init_cpu(numBRows, numBCols);
+    B->data = input1_data;
+    printf("Matrix B ready for solution! \n"); // GOOD!
+    
+    printf("Create Solution \n");
+    bin_matrix output = matrix_add_cpu(A,B);
 
-    compute(output_data, input0_data, input1_data, numARows, numACols, numBRows, numBCols, numCRows, numCCols);
-
+    
+    printf("Write Back To Files \n");
+    printf("Input0 \n");
     write_data(input0_file_name, input0_data, numARows, numACols);
+    printf("input0.raw READY! \n"); 
+    printf("Input1 \n");
     write_data(input1_file_name, input1_data, numBRows, numBCols);
-    write_data(output_file_name, output_data, numCRows, numCCols);
+    printf("input1.raw READY! \n");
+    printf("Output \n");
+    write_data(output_file_name, output->data, output->rows, output->cols);
+    printf("output.raw READY! \n");
 
     free(input0_data);
     free(input1_data);
     free(output_data);
 }
 
-int main()
-{
-    base_dir = wbPath_join(wbDirectory_current(), "MatrixMultiply", "Dataset");
+int main() {
+  base_dir = wbPath_join(wbDirectory_current(),
+                         "MatrixAdd", "Dataset");
 
-    create_dataset(0, 16, 16, 16);
-    create_dataset(1, 64, 64, 64);
-    create_dataset(2, 64, 128, 64);
-    create_dataset(3, 112, 48, 16);
-    create_dataset(4, 84, 84, 84);
-    create_dataset(5, 80, 99, 128);
-    create_dataset(6, 128, 128, 128);
-    create_dataset(7, 100, 100, 100);
-    create_dataset(8, 134, 130, 150);
-    create_dataset(9, 417, 210, 519);
+  create_dataset(0, 16, 16, 16, 16);
+  create_dataset(1, 4, 4, 4, 4);
   return 0;
 }
