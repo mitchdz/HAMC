758eb37a4a59a1ef0b39d1dbe8078265244d8a98
mitchdz <mitch_dz@hotmail.com>
2021-04-12 11:00:15 -0700

    add start to inverse matrix
    Signed-off-by: mitchdz <mitch_dz@hotmail.com>

--- src/tests/InverseMatrix/sources.cmake
+++ src/tests/InverseMatrix/sources.cmake
@@ -1,3 +1,3 @@
-add_lab("INVERSE_MATRIX_test")
-add_lab_solution("INVERSE_MATRIX_test", ${CMAKE_CURRENT_LIST_DIR}/main.cu)
-add_generator("INVERSE_MATRIX_test" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
+add_lab("inverse_test")
+add_lab_solution("inverse_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
+#add_generator("inverse_test" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
--- src/tests/sources.cmake
+++ src/tests/sources.cmake
@@ -1,2 +1,3 @@
 include(${CMAKE_CURRENT_LIST_DIR}/MultiplyMatrix/sources.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Transpose/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/InverseMatrix/sources.cmake)
