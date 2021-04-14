13dd0ddd21c9dd079df5bafc956fdf0947a105da
James Kuban <kubandj@email.arizona.edu>
2021-04-13 23:44:40 -0700

    Update decrypt.cu

--- src/hamc/decrypt.cu
+++ src/hamc/decrypt.cu
@@ -27,7 +27,7 @@ bin_matrix decode_gpu(bin_matrix word, mdpc code)
     printf("syn->rows: %d, syn->cols: %d, syn_cpu->rows: %d, syn_cpu->cols: %d\n", syn->rows, syn->cols, syn_cpu->rows, syn_cpu->cols);
     bool matching = true;
     for(int qw = 0; qw < syn->rows * syn->cols; qw++){
-        if(){
+        if(syn->data[qw] != syn_cpu->data[qw]){
             matching = false;
             printf("Position: [%d][%d]\n", syn->rows, syn->cols);
             break;
