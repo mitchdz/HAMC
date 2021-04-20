6e9f89031b39bfe7f990e3d0ca9a3a957387dfe0
James Kuban <kubandj@email.arizona.edu>
2021-04-19 23:16:02 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -185,7 +185,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         shortValue ^= pValue & 1;
         pValue >>= 8;
     }
-    C[Row * colB + Col] = pValue;
+    C[Row * colB + Col] = shortValue;
 }/**/
 
 __global__ void mult_kernel(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_t *B, HAMC_DATA_TYPE_t *C, int rowA, int rowB, int colA, int colB, int TILE_WIDTH)
