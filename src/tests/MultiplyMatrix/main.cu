9467eba97e76ef90c81a3be53597087c7eff871c
James Kuban <kubandj@email.arizona.edu>
2021-04-23 11:50:18 -0700

    Update main.cu

--- src/tests/MultiplyMatrix/main.cu
+++ src/tests/MultiplyMatrix/main.cu
@@ -187,7 +187,7 @@ void run_gpu_vers(int x, int y, int z)
             break;
         }
     }
-    /*for(int i = 0; i < G1->rows; i++){
+    for(int i = 0; i < G1->rows; i++){
         for(int j = 0; j < G1->cols; j++){
             printf("%d:", G1->data[i * G1->cols + j]);
             printf("%d, ", G2->data[i * G1->cols + j]);
