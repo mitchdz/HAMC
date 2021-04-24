f82badd3da88b929a1baca38b2cc91ab9fab50d6
James Kuban <kubandj@email.arizona.edu>
2021-04-23 21:57:56 -0700

    Update sources.cmake

--- src/tests/InverseMatrix/sources.cmake
+++ src/tests/InverseMatrix/sources.cmake
@@ -1,3 +1,3 @@
-add_lab("inverse_test")
-add_lab_solution("inverse_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
+#add_lab("inverse_test")
+#add_lab_solution("inverse_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
 #add_generator("inverse_test" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
