f7852dd58628154fc99bf420770490aec336e420
James Kuban <kubandj@email.arizona.edu>
2021-04-24 00:39:39 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -144,7 +144,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         }/**/
         __syncthreads();
         for(int j = 0; j < TILE_WIDTH; j++){
-            pValueFloat[0] ^= sharedFloatA[threadIdx.y * TILE_WIDTH + j] & transposeFloatB[threadIdx.x * TILE_WIDTH + j];
+            pValueFloat[0] ^= (sharedFloatA[threadIdx.y * TILE_WIDTH + j]) & (transposeFloatB[threadIdx.x * TILE_WIDTH + j]);
         }
     }
     //TODO: xor all pValue bits
