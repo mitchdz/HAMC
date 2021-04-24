f84b10826219fb1ee723f42c7b5f443c9c541c5c
James Kuban <kubandj@email.arizona.edu>
2021-04-23 21:56:17 -0700

    Update sources.cmake

--- src/tests/Transpose/sources.cmake
+++ src/tests/Transpose/sources.cmake
@@ -1,3 +1,3 @@
-add_lab("Transpose_test")
-add_lab_solution("Transpose_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
-add_generator("Transpose_test" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
+#add_lab("Transpose_test")
+#add_lab_solution("Transpose_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
+#add_generator("Transpose_test" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
