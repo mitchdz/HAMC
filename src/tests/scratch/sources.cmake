0a7ca6c57bcf2d337fcc476bc761719bfce52a5a
James Kuban <kubandj@email.arizona.edu>
2021-04-23 21:57:16 -0700

    Update sources.cmake

--- src/tests/scratch/sources.cmake
+++ src/tests/scratch/sources.cmake
@@ -1,3 +1,3 @@
-add_lab("scratch_test")
-add_lab_solution("scratch_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
+#add_lab("scratch_test")
+#add_lab_solution("scratch_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
 #add_generator("inverse_test" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
