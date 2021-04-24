3e3f52599ff958285589b0e41d14d3382e16daf4
James Kuban <kubandj@email.arizona.edu>
2021-04-23 23:15:07 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -129,10 +129,10 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
     
     for(int i = 0; i < ((colA - 1)/(TILE_WIDTH * 4)) + 1; i++){
         tilePos = i / 4 * TILE_WIDTH;
-        sharedFloatA[tid] = floatA[Row * colA + tilePos + threadIdx.x];
+        //sharedFloatA[tid] = floatA[Row * colA + tilePos + threadIdx.x];
         //sharedFloatB[tid] = floatB[((tilePos + threadIdx.y) * 4) * colB + Col];
         for(int j = 0; j < 4; j++){
-            //sharedB[tid * 4 + j] = B[(j + ((tilePos + threadIdx.y) * 4)) * colB + Col];
+            sharedB[tid * 4 + j] = B[(j + ((tilePos + threadIdx.y) * 4)) * colB + Col];
         }/**/
         __syncthreads();
         for(int j = 0; j < TILE_WIDTH; j++){
