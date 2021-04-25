2d6d3bacd495b42a3ed8e07706354e04e7d86ac7
James Kuban <kubandj@email.arizona.edu>
2021-04-24 23:23:36 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -201,7 +201,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
             //pValueFloat[0] ^= (sharedFloatA[threadIdx.y * TILE_WIDTH + j]) & (transposeFloatB[threadIdx.x * TILE_WIDTH + j]);
             pValueFloat ^= sharedFloatA[threadIdx.y * TILE_WIDTH + j] & sharedFloatB[threadIdx.x * TILE_WIDTH + j];
         }/**/
-        if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0 && i == 0){
+        if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0){
             uint32_t temp = 0;
             for(int q = 0; q < 32; q++){
                 temp ^= sharedFloatA[q] & sharedFloatB[q];
@@ -220,7 +220,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         /*for(int j = 0; j < TILE_WIDTH; j++){
                 shortValue ^= (sharedA[threadIdx.y * TILE_WIDTH + j] & sharedB[j * TILE_WIDTH + threadIdx.x]);
         }/**/
-        if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0 && i == ((colA - 1)/(TILE_WIDTH * 4))){
+        if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0){// && i == ((colA - 1)/(TILE_WIDTH * 4))){
         printf("pValueFloat: ");
             for(int j = 0; j < 32; j++){
                 char bit = (pValueFloat >> (31 - j)) & 1;
