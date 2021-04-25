448c0cd1edd80ca942bbc1848509edf363c4a036
James Kuban <kubandj@email.arizona.edu>
2021-04-24 20:46:27 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -182,7 +182,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         __syncthreads();
         
         if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0 && i == 0){
-            /*printf("A 0 through 3: ");
+            printf("A 0 through 3: ");
             for(int k = 0; k < 4; k++){
                 for(int j = 0; j < 8; j++){
                     char bit = (sharedA[tid + k] >> (7 - j)) & 1;
