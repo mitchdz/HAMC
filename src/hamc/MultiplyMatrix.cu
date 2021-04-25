c39dac704b5927dadf6e4898825d37dcd4164edf
James Kuban <kubandj@email.arizona.edu>
2021-04-25 04:14:51 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -244,7 +244,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         }
         __syncthreads();
         
-        if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0){// && i == 0){
+        //if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0){// && i == 0){
             /*printf("A: i = %d\n", i);
             
             for(int q = 0; q < 32; q++){
@@ -257,7 +257,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
                 }
                 printf("\n");
             }/**/
-            printf("B: i = %d\n", i);
+            //printf("B: i = %d\n", i);
             /*for(int q = 0; q < 32; q++){
                 for(int jk = 0; jk < 4; jk++){
                     for(int k = 0; k < 32; k++){
@@ -267,7 +267,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
                 }
                 printf("\n");
             }/**/
-            for(int q = 0; q < 32; q++){
+            /*for(int q = 0; q < 32; q++){
                     for(int k = 0; k < 32; k++){
                         for(int asd = 0; asd < 4; asd++){
                             char bit = (sharedFloatB[q * (TILE_WIDTH + 1) + tid + k] >> (31 - 8 * asd)) & 1;
@@ -286,7 +286,7 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
                 printf(" ");
             }
             printf("\n");/**/
-        }
+        //}
         for(int j = 0; j < TILE_WIDTH; j++){
             pValueFloat ^= sharedFloatA[threadIdx.y * TILE_WIDTH + j] & sharedFloatB[threadIdx.x * (TILE_WIDTH + 1) + j];
         }/**/
@@ -482,7 +482,7 @@ __global__ void mult_kernel_debug(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_t *B, HAMC
         }
         __syncthreads();
         
-        if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0){// && i == 0){
+        //if(blockIdx.x == 0 && blockIdx.y == 0 && tid == 0){// && i == 0){
             /*printf("A: i = %d\n", i);
             for(int q = 0; q < 32; q++){
                 for(int k = 0; k < 32; k++){
@@ -491,7 +491,7 @@ __global__ void mult_kernel_debug(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_t *B, HAMC
                 }
                 printf("\n");
             }/**/
-            printf("B: i = %d\n", i);
+            /*printf("B: i = %d\n", i);
             for(int q = 0; q < 32; q++){
                 for(int k = 0; k < 32; k++){
                     char bit = (sharedB[k * TILE_WIDTH + tid + q]) & 1;
@@ -499,7 +499,7 @@ __global__ void mult_kernel_debug(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_t *B, HAMC
                 }
                 printf("\n");
             }/**/
-        }
+        //}
         
         if((Row < rowA) && (Col < colB)){
             for(int j = 0; j < TILE_WIDTH; j++){
