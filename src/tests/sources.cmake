bbdaadde6618e4ede72890fdaf81a650d9d7b4a9
Mitch-R <70560907+M-Russell-Code@users.noreply.github.com>
2021-04-25 12:51:47 -0400

    Update sources.cmake

--- src/tests/sources.cmake
+++ src/tests/sources.cmake
@@ -1,1 +1,9 @@
-include(${CMAKE_CURRENT_LIST_DIR}/RREFMatrix/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/MultiplyMatrix/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/Transpose/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/InverseMatrix/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/LU_inverse/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/LU_inverse_block/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/sharma/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/MatrixAdd/sources.cmake)
+
+
