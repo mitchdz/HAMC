daa229ced2040d4ff7dc1f1fc84329e58c68aca8
James Kuban <kubandj@email.arizona.edu>
2021-04-28 21:11:58 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -234,14 +234,14 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         
         if((Row < rowA) && (tilePos + threadIdx.x < colA / 4)){
             sharedFloatA[tid] = floatA[Row * colA / 4 + tilePos + threadIdx.x];
-            if(tilePos + threadIdx.x + 3 > colA / 4){
+            /*if(tilePos + threadIdx.x + 3 > colA / 4){
             //int padding = (colA / 4) - (tilePos + threadIdx.x + 3);
             int padding = 4 - colA % 4;
             //printf("Padding: %d\n", padding);
                 for(int j = 1; j <= padding; j++){
                     sharedA[j] = 0;
                 }
-            }
+            }/**/
         }
         else{
             sharedFloatA[tid] = (uint32_t)0;
