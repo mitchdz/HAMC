6c2c2fdb1b2c4012153805710f24c893d4da862a
Mitch-R <70560907+M-Russell-Code@users.noreply.github.com>
2021-05-05 15:12:58 -0400

    Update sources.cmake

--- src/tests/MatrixAdd/sources.cmake
+++ src/tests/MatrixAdd/sources.cmake
@@ -1,3 +1,3 @@
 add_lab("MatrixAdd")
 add_lab_solution("MatrixAdd_test" ${CMAKE_CURRENT_LIST_DIR}/main.cu)
-add_generator("MatrixAdd" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
+//add_generator("MatrixAdd" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
