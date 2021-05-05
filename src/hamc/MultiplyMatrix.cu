935c6aaa684fc0285a093b79dea6e161327d48f4
James Kuban <kubandj@email.arizona.edu>
2021-05-04 20:30:04 -0700

    Update MultiplyMatrix.cu

--- src/hamc/MultiplyMatrix.cu
+++ src/hamc/MultiplyMatrix.cu
@@ -16,26 +16,35 @@
 
 __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_t *B, HAMC_DATA_TYPE_t *C, int rowA, int rowB, int colA, int colB)
 {
+    //Shared variable created
     __shared__ uint32_t sharedFloatA[TILE_WIDTH * TILE_WIDTH];
     __shared__ uint32_t sharedFloatB[TILE_WIDTH * (TILE_WIDTH + 1)];
+    //Shared variable typecast for later use
     HAMC_DATA_TYPE_t *sharedA = (uint8_t *)sharedFloatA;
     HAMC_DATA_TYPE_t *sharedB = (uint8_t *)sharedFloatB;
     
+    //Typecasting of global variables for consolidated data accesses
     uint32_t *floatA = (uint32_t *)A;
     uint32_t *floatB = (uint32_t *)B;
     
+    //Indecies definitions
     int Row = blockIdx.y * TILE_WIDTH + threadIdx.y;
     int Col = blockIdx.x * TILE_WIDTH + threadIdx.x;
     int tid = threadIdx.y * TILE_WIDTH + threadIdx.x;
     int tilePos = 0;
     
+    //Local output variables
     uint32_t pValueFloat = 0;
     HAMC_DATA_TYPE_t shortValue = 0;
     
+    //For loop for tiled access
     for(int i = 0; i < ((colA - 1)/(TILE_WIDTH * 4)) + 1; i++){
+        //Tile indecies
         tilePos = i * TILE_WIDTH;
+        //Boundary checking for global matrix A
         if((Row < rowA) && (tilePos + threadIdx.x < (colA - 1) / 4 + 1)){
             memcpy(&sharedFloatA[tid], &A[Row * colA + tilePos * 4 + threadIdx.x * 4], sizeof(uint32_t));
+            //Further boundary conditions to handle boundaries inside the last global read
             if((tilePos + threadIdx.x) == colA / 4){
                 int padding = colA % 4;
                 for(int j = 3; j >= padding; j--){
@@ -46,8 +55,10 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         else{
             sharedFloatA[tid] = (uint32_t)0;
         }
+        //Matrix B still has to be accesed without consolidation
         for(int j = 0; j < 4; j++){
             #pragma unroll
+            //Boundary checking for global matrix B
             if(((j * TILE_WIDTH + threadIdx.y + tilePos * 4) < rowB) && (Col < colB)){
                 sharedB[threadIdx.x * 4 * (TILE_WIDTH + 1) + j * (TILE_WIDTH) + threadIdx.y] = B[colB * (j * TILE_WIDTH + threadIdx.y + tilePos * 4) + Col];
             }
@@ -57,14 +68,17 @@ __global__ void mult_kernel_compressed_data(HAMC_DATA_TYPE_t *A, HAMC_DATA_TYPE_
         }        
         __syncthreads();
         
+        //Binary matrix multiplication performed for each tile
         for(int j = 0; j < TILE_WIDTH; j++){
             #pragma unroll
             pValueFloat ^= sharedFloatA[threadIdx.y * TILE_WIDTH + j] & sharedFloatB[threadIdx.x * (TILE_WIDTH + 1) + j];
         }
         __syncthreads();
     }
-
+    
+    //Output boundary conditions
     if(Row < rowA && Col < colB){
+        //Final step of matrix multiplication
         for(int i = 0; i < 4; i++){
             #pragma unroll
             shortValue ^= pValueFloat & 1;
